8343b07411d61d687398ff3918fa627be2281979
CSE360-Team5-Lin <zbao7@asu.edu>
2018-07-08 10:36:15 -0700

    build a andriod framwork

---
+++ app/src/main/java/com/example/zelinbao/cse535assign1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.zelinbao.cse535assign1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
